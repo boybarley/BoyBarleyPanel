42b3031270f7e7ed2f699f6da6f08df3230c7416
boybarley <boybarley@boybarley.com>
2025-02-03 12:19:48 +1100

    Create setup.sh

---
+++ deploy/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Basic setup script
+
+echo "Installing dependencies..."
+sudo apt update && sudo apt install -y python3-venv python3-dev libpq-dev
+
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Installing Python packages..."
+pip install -r requirements.txt
+
+echo "Setting up database..."
+flask db init
+flask db migrate
+flask db upgrade
+
+echo "Creating admin user..."
+flask create-admin
+
+echo "Installation completed!"
